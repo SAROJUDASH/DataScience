81e369a976ef06b01761e008952b064674945f8c
SAROJUDASH <144780292+SAROJUDASH@users.noreply.github.com>
2025-08-14 18:14:44 +0545

    Add files via upload

---
+++ basicpython.py
@@ -0,0 +1,23 @@
+#print Hello World
+print("Hello World")
+
+#user input
+name = input("Enter your name:")
+print(name)
+
+#type casting
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the another number:"))
+print(num1 + num2)
+
+print(int("56"))
+print(float(12))
+print(str(56))
+print(int("Saroj Udash"))
+print(int(5.25))
+
+#Data Type
+print(type(123))
+print(type("Sa Roj"))
+print(type(22.98))
+
---
+++ task.py
@@ -0,0 +1,16 @@
+#task1: WAP to print your name
+print("Saroj udash")
+
+#task2: Input a fav player name and print it
+name = input("Enter your favorite player name:")
+print(name)
+
+#task3: Input for every data type we discussed and print it
+num = int(input("Enter any integer:"))
+print(num)
+
+name = input("Enter any string:")
+print(name)
+
+num1 =float(input("Enter any floating number:"))
+print(num1)
